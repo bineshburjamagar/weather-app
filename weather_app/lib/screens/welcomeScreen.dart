1af7b3bf8e00a01e23ab7910921b031de487669f
Binesh Burja Magar <bineshburzaa@gmail.com>
2022-05-19 23:44:11 +0545

    data fetching and json decoding

--- weather_app/lib/screens/welcomeScreen.dart
+++ weather_app/lib/screens/welcomeScreen.dart
@@ -5,6 +5,8 @@ import 'package:flutter/material.dart';
 import 'package:weather_app/services/location.dart';
 import 'package:lottie/lottie.dart';
 import 'package:glassmorphism/glassmorphism.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class WelcomeScreen extends StatefulWidget {
   const WelcomeScreen({Key? key}) : super(key: key);
@@ -18,6 +20,7 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
   void initState() {
     super.initState();
     getLocation();
+    getData();
   }
 
   void getLocation() {
@@ -29,6 +32,18 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
     print(location.longitude);
   }
 
+  void getData() async {
+    http.Response response = await http
+        .get(Uri.parse('https://goweather.herokuapp.com/weather/Pokhara'));
+    String data = response.body;
+    var decodeData = jsonDecode(data);
+    var temp = decodeData['temperature'];
+    print(temp);
+    var desc = decodeData['description'];
+    print(desc);
+    print(response.statusCode);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,76 +53,96 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
             'https://assets9.lottiefiles.com/packages/lf20_boJRmE.json',
             fit: BoxFit.cover,
             height: double.infinity),
-        Padding(
-          padding: const EdgeInsets.only(top: 30.0),
-          child: Row(
-            children: [
-              Column(
-                children: [
-                  const Text(
-                    'NEPAL',
-                    style: TextStyle(
-                        color: Color(0xFF300016),
-                        fontSize: 30.00,
-                        fontWeight: FontWeight.bold,
-                        fontFamily: 'Grape Nuts'),
-                  ),
-                  const SizedBox(
-                    height: 10.0,
-                  ),
-                  const Padding(
-                    padding: EdgeInsets.only(left: 50.0),
-                    child: Text('28°',
-                        style: TextStyle(
-                            color: Color(0xFFDA9B98), fontSize: 150.00)),
-                  ),
-                  GlassmorphicContainer(
-                      padding: const EdgeInsets.fromLTRB(0.0, 400, 0.0, 0.0),
-                      margin: const EdgeInsets.fromLTRB(0.0, 400, 0.0, 0.0),
-                      width: 200,
-                      height: 100,
-                      borderRadius: 20,
-                      blur: 20,
-                      alignment: Alignment.bottomCenter,
-                      border: 2,
-                      linearGradient: LinearGradient(
-                          begin: Alignment.topLeft,
-                          end: Alignment.bottomRight,
-                          colors: [
-                            const Color(0xFFffffff).withOpacity(0.1),
-                            const Color(0xFFFFFFFF).withOpacity(0.05),
-                          ],
-                          stops: const [
-                            0.1,
-                            1,
-                          ]),
-                      borderGradient: LinearGradient(
-                        begin: Alignment.topLeft,
-                        end: Alignment.bottomRight,
-                        colors: [
-                          const Color(0xFFffffff).withOpacity(0.5),
-                          const Color((0xFFFFFFFF)).withOpacity(0.5),
-                        ],
-                      )),
+        MainTitle(),
+        GlassmorphicContainer(
+            // padding: const EdgeInsets.fromLTRB(0.0, 400, 0.0, 0.0),
+            margin: const EdgeInsets.fromLTRB(40.0, 650.0, 0.0, 0.0),
+            width: 350,
+            height: 100,
+            borderRadius: 20,
+            blur: 20,
+            alignment: Alignment.bottomCenter,
+            border: 2,
+            linearGradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [
+                  const Color(0xFFffffff).withOpacity(0.1),
+                  const Color(0xFFFFFFFF).withOpacity(0.05),
                 ],
+                stops: const [
+                  0.1,
+                  1,
+                ]),
+            borderGradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                const Color(0xFFffffff).withOpacity(0.5),
+                const Color((0xFFFFFFFF)).withOpacity(0.5),
+              ],
+            )),
+      ]),
+    ));
+  }
+}
+
+class MainTitle extends StatefulWidget {
+  MainTitle({Key? key}) : super(key: key);
+
+  @override
+  State<MainTitle> createState() => _MainTitleState();
+}
+
+class _MainTitleState extends State<MainTitle> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(top: 50.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: const [
+              Text(
+                'NEPAL',
+                style: TextStyle(
+                    color: Color(0xFF300016),
+                    fontSize: 30.00,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Grape Nuts'),
               ),
-              Container(
-                  height: 50.0,
-                  margin: const EdgeInsets.fromLTRB(10.0, 90, 0.0, 0.0),
-                  alignment: Alignment.topCenter,
-                  child: Transform.rotate(
-                    angle: -math.pi / 2,
-                    child: const Text('Clear skies',
-                        style: TextStyle(
-                            color: Color(0xFF835496),
-                            fontSize: 30.00,
-                            fontWeight: FontWeight.w400,
-                            fontFamily: 'Grape Nuts')),
-                  )),
+              SizedBox(
+                height: 10.0,
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 50.0),
+                child: Text('28°',
+                    style:
+                        TextStyle(color: Color(0xFFDA9B98), fontSize: 150.00)),
+              ),
             ],
           ),
         ),
-      ]),
-    ));
+        Column(
+          children: [
+            Container(
+                height: 100.0,
+                margin: const EdgeInsets.fromLTRB(20.0, 120.0, 0.0, 20.0),
+                // padding: const EdgeInsets.fromLTRB(0.0, 0, 0.0, 50.0),
+                alignment: Alignment.topCenter,
+                child: Transform.rotate(
+                  angle: -math.pi / 2,
+                  child: const Text('Clear skies',
+                      style: TextStyle(
+                          color: Color(0xFF835496),
+                          fontSize: 30.00,
+                          fontWeight: FontWeight.w400,
+                          fontFamily: 'Grape Nuts')),
+                )),
+          ],
+        ),
+      ],
+    );
   }
 }
